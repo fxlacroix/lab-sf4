9ff19fad175c1c85d52aa08ddf88a3c2dedbbd5e
François-Xavier LACROIX <fxlacroix@gmail.com>
2019-08-26 16:08:51 +0200

    hot reload css js

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -1,10 +1,24 @@
 <?php
 
-
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class GameController
 {
 
+    /**
+     * @Route("/game")
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index(Environment $twig)
+    {
+        return new Response($twig->render("test.html.twig"));
+    }
 }
